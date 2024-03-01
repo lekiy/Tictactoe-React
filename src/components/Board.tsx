e5f35a72b50cf51b8edc3c2ecb6630795f9cf789
Kevin Annen <lekiyminin@gmail.com>
2024-02-29 23:22:39 -0600

    working on win detection

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,6 @@
 import Square from "./Square";
 import "./Board.css";
+import { useCallback, useState } from "react";
 
 type BoardProps = {
   currentPlayer: number;
@@ -7,11 +8,49 @@ type BoardProps = {
 };
 
 const Board: React.FC<BoardProps> = ({ currentPlayer, nextTurn }) => {
-  const squares = Array(9).fill(
-    <Square currentPlayer={currentPlayer} nextTurn={nextTurn} />
+  const [gridCells, setGridCells] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const squares = [];
+
+  const checkForWin = useCallback(
+    (currentPlayer: number) => {
+      console.log(currentPlayer);
+      if ((gridCells[0] + gridCells[1] + gridCells[2]) / 3 === currentPlayer)
+        return true;
+      return false;
+    },
+    [gridCells]
   );
 
-  return <div className="board">{squares}</div>;
+  const handleUpdateCell = useCallback(
+    (position: number, value: number) => {
+      setGridCells(
+        gridCells.map((cell, index) => {
+          if (index === position) return value;
+          return cell;
+        })
+      );
+      console.log(checkForWin(value));
+    },
+    [checkForWin, gridCells]
+  );
+
+  for (let index = 0; index < gridCells.length; index++) {
+    squares.push(
+      <Square
+        currentPlayer={currentPlayer}
+        nextTurn={nextTurn}
+        position={index}
+        updateCell={handleUpdateCell}
+      />
+    );
+  }
+
+  return (
+    <div className="board">
+      {squares}
+      {gridCells}
+    </div>
+  );
 };
 
 export default Board;
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -3,26 +3,38 @@ import { useCallback, useState } from "react";
 type SquareProps = {
   currentPlayer: number;
   nextTurn: () => void;
+  updateCell: (position: number, value: number) => void;
+  position: number;
 };
 
-const Square: React.FC<SquareProps> = ({ currentPlayer, nextTurn }) => {
+const Square: React.FC<SquareProps> = ({
+  currentPlayer,
+  nextTurn,
+  updateCell,
+  position,
+}) => {
   const [value, setValue] = useState(0);
   const handleOnClickSquare = useCallback(() => {
     setValue(currentPlayer);
+    updateCell(position, currentPlayer);
     nextTurn();
-  }, [currentPlayer, nextTurn]);
+  }, [currentPlayer, nextTurn, position, updateCell]);
 
-  const getGamePiece = useCallback((value: number) => {
-    if (value == 1) return "O";
-    if (value == 2) return "X";
-  }, []);
+  const getGamePiece = useCallback(
+    (value: number) => {
+      if (value == 1) return "O";
+      if (value == 2) return "X";
+      if (value == 0) return String(position);
+    },
+    [position]
+  );
 
   return (
     <div>
       {value ? (
         getGamePiece(value)
       ) : (
-        <button onClick={handleOnClickSquare}>.</button>
+        <button onClick={handleOnClickSquare}>{getGamePiece(value)}</button>
       )}
     </div>
   );
